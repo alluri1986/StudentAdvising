bbcef288e271bf5f5e70f1eebcdfb2d4bee17610
appasani.rajesh@gmail.com <appasani.rajesh@gmail.com>
2012-03-14 03:23:04 -0500

    test

---
+++ StudentAdvising.Common/CommonBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StudentAdvising.Common
+{
+     public virtual class CommonBase
+    {
+        virtual int ID { get; set; }
+
+        bool IsActiveFL { get; set; }
+
+        DateTime LastUpdatedDate { get; set; }
+
+        DateTime CreationDate { get; set; }
+
+        int CreatedBy { get; set; }
+
+        int LastUpdatedBy { get; set; }
+
+    }
+}
