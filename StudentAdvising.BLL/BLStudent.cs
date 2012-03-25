c99b45f216787ebb79566d9ed352880b7c56a454
pratap2035 <alluri.pratap@gmail.com>
2012-03-25 15:23:15 -0500

    te4

--- StudentAdvising.BLL/BLStudent.cs
+++ StudentAdvising.BLL/BLStudent.cs
@@ -4,11 +4,21 @@ using System.Linq;
 using System.Text;
 using StudentAdvising.DLL;
 
+
 namespace StudentAdvising.BLL
 {
     class BLStudent
     {
-        
+
+        private DLStudent dlStudent = null;
+
+        public DLStudent GetDLStudent()
+        {
+            if (dlStudent == null)
+                dlStudent = new DLStudent();
+
+            return dlStudent;
+        }
 
     }
 }
