6affb14cf8be59bb7b69681863995a9e3b7a8191
pratap2035 <alluri.pratap@gmail.com>
2012-04-03 22:03:03 -0500

    k

--- StudentAdvising.BLL/BLStudent.cs
+++ StudentAdvising.BLL/BLStudent.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using StudentAdvising.DLL;
+using StudentAdvising.Common;
 
 
 namespace StudentAdvising.BLL
@@ -18,6 +19,27 @@ namespace StudentAdvising.BLL
                 dlStudent = new DLStudent();
 
             return dlStudent;
+        }
+
+
+        /// <summary>
+        /// This function is used for insert and updated functionality for student
+        /// </summary>
+        /// <param name="student"></param>
+        /// <returns></returns>
+        public Student SaveStudent(Student student)
+        {
+            try
+            {
+                student.CreationDate = DateTime.UtcNow;
+                student.LastUpdatedDate = DateTime.UtcNow;
+                GetDLStudent().SaveStudent(student);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message.ToString());
+            }
+            return student;
         }
 
     }
